6fd5b0c654b912c00ed42024208afc6b3bda1171
Gaoyang Zhang <younglobalization@gmail.com>
2026-06-24 18:17:06 -0700

    Revert "ci: skip mac dependency collection in electron builder"
    This reverts commit 53c150665cde014a2120672ec77932b8390ea9db.

--- apps/desktop/scripts/before-build.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = async function beforeBuild(context) {
-  if (context.platform?.nodeName === "darwin") {
-    console.log("[before-build] macOS node_modules are prepared by pnpm install/rebuild-native; skipping electron-builder dependency collection.");
-    return false;
-  }
-
-  return true;
-};
